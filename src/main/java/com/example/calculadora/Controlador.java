b5557119d11b912208c26845ea42300ca59fb230
brunales21 <bemimarket@gmail.com>
2023-11-08 19:33:01 +0100

    mejoras de todo tipo (calculadora indestructible)

--- src/main/java/com/example/calculadora/Controlador.java
+++ src/main/java/com/example/calculadora/Controlador.java
@@ -13,6 +13,9 @@ public class Controlador {
 
     @FXML private TextField textField;
     private CalculadoraModelo modelo;
+    @FXML private Button cierraParentesis;
+
+
 
     public Controlador() {
         this.modelo = new CalculadoraModelo();
@@ -23,10 +26,11 @@ public class Controlador {
         Button btn = ((Button) mouseEvent.getSource());
         String btnText = btn.getText();
 
+
         if (isPartOfExpression(btn.getText())) {
             insertToTextField(btnText);
         } else if (presionoResolver(btnText)) {
-            resolver(textField.getText());
+            resolver(fixExpression(textField.getText()));
         } else if (presionoLimpiar(btnText)) {
             vaciar();
         } else {
@@ -38,6 +42,14 @@ public class Controlador {
         }
     }
 
+    public void enableButton(Button btn) {
+        btn.setDisable(false);
+
+    }
+
+    public void disableButton(Button b) {
+        b.setDisable(true);
+    }
     public boolean presionoResolver(String btnText) {
         return btnText.equals("=");
     }
@@ -57,38 +69,82 @@ public class Controlador {
         return String.valueOf(valor.charAt(valor.length()-1));
     }
 
+    public int ocurrenciasDe(String c) {
+        int contador = 0;
+        for (int i = 0; i < textField.getText().length(); i++) {
+            String elemento = String.valueOf(textField.getText().charAt(i));
+            if (elemento.equals(c)) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+
+
     public void insertToTextField(String input) {
+
         if (isOperador(input) && isOperador(getLastPos(textField.getText()))) {
             textField.setText(textField.getText().substring(0, textField.getText().length()-1).concat(input));
             return;
         }
+
         textField.appendText(input);
+        Button cierraParentesisBtn = (Button) this.cierraParentesis.getScene().lookup("#cierraParentesis");
+
+        if (input.equals(")") && ocurrenciasDe(")") == ocurrenciasDe("(")) {
+            disableButton(cierraParentesisBtn);
+        } else if (input.equals("(") && ocurrenciasDe("(") > ocurrenciasDe(")")) {
+            enableButton(cierraParentesisBtn);
+        }
+
+
     }
 
-
+    public String fixExpression(String expresion) {
+        if (getCantParentesisRestantes(expresion) != 0) {
+            expresion = fillWithParentesis(expresion);
+        }
+        return expresion;
+    }
 
     public void resolver(String expresion) {
         String m;
         try {
-            textField.setText(modelo.calcular(expresion));
+
+            String resultado = modelo.calcular(expresion);
+            System.out.println(resultado);
+            String decimalPart = resultado.substring(resultado.indexOf(".")+1);
+            System.out.println(decimalPart);
+            if (isEntero(decimalPart)) {
+                textField.setText(getParteEntera(resultado));
+            } else {
+                textField.setText(resultado);
+            }
             return;
         } catch (ArithmeticException e) {
             m = "No se puede dividir entre 0, es infinito.";
-        } catch (EmptyStackException | IllegalArgumentException e) {
-            m = "Faltan o sobran parentesis";
         }
         instanceVentanaError(m);
     }
 
+    public int getCantParentesisRestantes(String expresion) {
+        return ocurrenciasDe("(") - ocurrenciasDe(")");
+    }
+
+    public String fillWithParentesis(String expresion) {
+        for (int i = 0; i < getCantParentesisRestantes(expresion); i++) {
+            expresion = expresion.concat(")");
+        }
+        System.out.println(expresion);
+        return expresion;
+    }
     public void instanceVentanaError(String mensaje) {
         Alert alerta = new Alert(Alert.AlertType.ERROR);
         alerta.setTitle("");
         alerta.setHeaderText(null);
         alerta.setContentText(mensaje);
         alerta.showAndWait();
-    }
-    public boolean isValidExpression(String expression) {
-        return !expression.contains("x/") && !expression.contains("/x") && !expression.contains("+x") && !expression.contains("-x") && !expression.contains("-/") && !expression.contains("+/") && !expression.contains("//") && !expression.contains("xx") && !expression.contains("..");
     }
 
     public void vaciar(){
@@ -100,20 +156,18 @@ public class Controlador {
 
     }
 
-    /*
-    public boolean cantParentesisPar(String expresion) {
-        int a = 0;
-        int b = 0;
-        for (int i = 0; i < expresion.length(); i++) {
-            char c = expresion.charAt(i);
-            if (c == '(') {
-                a++;
-            } else if (c == ')') {
-                b++;
+    public boolean isEntero(String decPart) {
+        for (int i = 0; i < decPart.length(); i++) {
+            if (decPart.charAt(i) != '0') {
+                return false;
             }
         }
-        return a == b;
+        return true;
     }
-     */
+
+    public String getParteEntera(String num) {
+        return num.substring(0, num.indexOf("."));
+    }
+
 }
 
